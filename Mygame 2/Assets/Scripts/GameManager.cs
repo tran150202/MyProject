ebd19b4843c1c1b18d7b1c3305ce80141d194bc3
tran150202 <144520072+tran150202@users.noreply.github.com>
2024-04-18 18:55:47 +0700

    files upload

---
+++ Mygame 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+using Completed;        //Allows us to use Lists. 
+
+public class GameManager : MonoBehaviour
+{
+    //Time to wait before starting level, in seconds.
+    public float levelStartDelay = 2f;
+    //Delay between each Player turn.
+    public float turnDelay = 0.1f;
+    //Starting value for Player food points.
+    public int playerFoodPoints = 100;
+    //Static instance of GameManager which allows it to be accessed by any other script.
+    public static GameManager instance = null;
+    //Boolean to check if it's players turn, hidden in inspector but public.
+    [HideInInspector] public bool playersTurn = true;
+
+    //Store a reference to our BoardManager which will set up the level
+    private BoardManager boardScript;
+    //Current level number, expressed in game as "Day 1".
+    private int level = 1;
+    //List of all Enemy units, used to issue them move commands.
+    private List<Enemy> enemies;
+    //Boolean to check if enemies are moving.
+    private bool enemiesMoving;
+
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    static public void CallbackInitialization()
+    {
+        //register the callback to be called everytime the scene is loaded
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    //This is called each time a scene is loaded.
+    static private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        instance.level++;
+        instance.InitGame();
+    }
+
+    //This is called each time a scene is loaded.
+    void OnLevelWasLoaded(int index)
+    {
+        //Add one to our level number.
+        level++;
+        //Call InitGame to initialize our level.
+        InitGame();
+    }
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+
+        //Clear any Enemy objects in our List to prepare for next level.
+        enemies.Clear();
+
+        //Call the SetupScene function of the BoardManager script, pass it current level number.
+        boardScript.SetupScene(level);
+
+    }
+
+
+    //Update is called every frame.
+    void Update()
+    {
+        //Check that playersTurn or enemiesMoving or doingSetup are not currently true.
+        if (playersTurn || enemiesMoving)
+
+            //If any of these are true, return and do not start MoveEnemies.
+            return;
+
+        //Start moving enemies.
+        StartCoroutine(MoveEnemies());
+    }
+
+    //Call this to add the passed in Enemy to the List of Enemy objects.
+    public void AddEnemyToList(Enemy script)
+    {
+        //Add Enemy to List enemies.
+        enemies.Add(script);
+    }
+
+
+    //GameOver is called when the player reaches 0 food points
+    public void GameOver()
+    {
+
+        //Enable black background image gameObject.
+  //      levelImage.SetActive(true);
+
+        //Disable this GameManager.
+        enabled = false;
+    }
+
+    //Coroutine to move enemies in sequence.
+    IEnumerator MoveEnemies()
+    {
+        //While enemiesMoving is true player is unable to move.
+        enemiesMoving = true;
+
+        //Wait for turnDelay seconds, defaults to .1 (100 ms).
+        yield return new WaitForSeconds(turnDelay);
+
+        //If there are no enemies spawned (IE in first level):
+        if (enemies.Count == 0)
+        {
+            //Wait for turnDelay seconds between moves, replaces delay caused by enemies moving when there are none.
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        //Loop through List of Enemy objects.
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            //Call the MoveEnemy function of Enemy at index i in the enemies List.
+            enemies[i].MoveEnemy();
+
+            //Wait for Enemy's moveTime before moving next Enemy, 
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+        //Once Enemies are done moving, set playersTurn to true so player can move.
+        playersTurn = true;
+
+        //Enemies are done moving, set enemiesMoving to false.
+        enemiesMoving = false;
+    }
+}
